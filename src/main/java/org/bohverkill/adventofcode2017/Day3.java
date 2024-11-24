c2a39da9e446a67a83d77ab47356d6ecd8612c48
BOHverkill <bohverkill@bohverkill.org>
2024-11-24 20:30:42 +0100

    Add solution for day 3 2017

--- src/main/java/org/bohverkill/adventofcode2017/Day3.java
+++ src/main/java/org/bohverkill/adventofcode2017/Day3.java
@@ -2,6 +2,8 @@ package org.bohverkill.adventofcode2017;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Day3 {
@@ -26,28 +28,110 @@ public class Day3 {
         //System.out.println(diagonal1 - input);
         //System.out.println(input - diagonal2);
 
-        int line1 = (int) (4 * Math.pow(center, 2) - 3 * center + 1);
-        int line2 = (int) (4 * Math.pow(center, 2) - center + 1);
-        int line3 = (int) (4 * Math.pow(center, 2) + center + 1);
+//        int line1 = (int) (4 * Math.pow(center, 2) - 3 * center + 1);
+//        int line2 = (int) (4 * Math.pow(center, 2) - center + 1);
+//        int line3 = (int) (4 * Math.pow(center, 2) + center + 1);
         int line4 = (int) (4 * Math.pow(center, 2) + 3 * center + 1);
 
-        System.out.println(line1 - input);
-        System.out.println(line2 - input);
-        System.out.println(line3 - input);
-        System.out.println(line4 - input);
+//        System.out.println(line1 - input);
+//        System.out.println(line2 - input);
+//        System.out.println(line3 - input);
+//        System.out.println(line4 - input);
 
-        System.out.println(line4 - input + center);
+        System.out.println("Part 1: " + (line4 - input + center));
     }
 
     private static void task2(final int input) {
-        int i = 2;
-        int j = 1;
-        int[] prev = new int[3];
-        prev[0] = 1;
-        while(j <= input) {
-            j += prev[0];
-            i++;
+        Node first = Node.of(1, Direction.RIGHT);
+        Node second = Node.of(1, Direction.UP);
+        first.neighbors()[4] = second;
+        second.neighbors()[3] = first;
+        Node current = second;
+        while (current.value() <= input) {
+            int directionIndex = switch (current.direction()) {
+                case RIGHT -> 1;
+                case UP -> 3;
+                case LEFT -> 6;
+                case DOWN -> 4;
+            };
+            Direction nextDirection; // check if left node is null/empty
+            if (current.neighbors()[directionIndex] == null) {
+                nextDirection = current.direction().next();
+            } else {
+                nextDirection = current.direction();
+            }
+            Node[] nextNeighbors = Node.newNeighbors();
+            switch (nextDirection) {
+                case RIGHT -> {
+                    nextNeighbors[3] = current;
+                    nextNeighbors[0] = current.neighbors()[1];
+                    nextNeighbors[1] = current.neighbors()[2];
+                    if (current.neighbors()[2] != null) {
+                        nextNeighbors[2] = current.neighbors()[2].neighbors()[4];
+                    }
+                }
+                case UP -> {
+                    nextNeighbors[6] = current;
+                    nextNeighbors[5] = current.neighbors()[3];
+                    nextNeighbors[3] = current.neighbors()[0];
+                    if (current.neighbors()[0] != null) {
+                        nextNeighbors[0] = current.neighbors()[0].neighbors()[1];
+                    }
+                }
+                case LEFT -> {
+                    nextNeighbors[4] = current;
+                    nextNeighbors[7] = current.neighbors()[6];
+                    nextNeighbors[6] = current.neighbors()[5];
+                    if (current.neighbors()[5] != null) {
+                        nextNeighbors[5] = current.neighbors()[5].neighbors()[3];
+                    }
+                }
+                case DOWN -> {
+                    nextNeighbors[1] = current;
+                    nextNeighbors[2] = current.neighbors()[4];
+                    nextNeighbors[4] = current.neighbors()[7];
+                    if (current.neighbors()[7] != null) {
+                        nextNeighbors[7] = current.neighbors()[7].neighbors()[6];
+                    }
+                }
+            }
+            int value = Arrays.stream(nextNeighbors).filter(Objects::nonNull).mapToInt(Node::value).sum();
+            Node next = new Node(value, nextNeighbors, nextDirection);
+            switch (nextDirection) {
+                case RIGHT -> current.neighbors()[4] = next;
+                case UP -> current.neighbors()[1] = next;
+                case LEFT -> current.neighbors()[3] = next;
+                case DOWN -> current.neighbors()[6] = next;
+            }
+            current = next;
         }
-        System.out.println(i);
+
+        System.out.println("Part 2: " + current.value());
+    }
+
+    public enum Direction {
+        RIGHT, UP, LEFT, DOWN;
+
+        public Direction next() {
+            return switch (this) {
+                case RIGHT -> UP;
+                case UP -> LEFT;
+                case LEFT -> DOWN;
+                case DOWN -> RIGHT;
+            };
+        }
+    }
+
+    private record Node(int value, Node[] neighbors, Direction direction) {
+        // 0 1 2
+        // 3   4
+        // 5 6 7
+        public static Node of(int value, Direction direction) {
+            return new Node(value, newNeighbors(), direction);
+        }
+
+        public static Node[] newNeighbors() {
+            return new Node[8];
+        }
     }
 }
