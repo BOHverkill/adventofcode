f9c40e8ca311abbe8c19e3ed9df07054502140e1
BOHverkill <bohverkill@bohverkill.org>
2025-11-30 09:33:49 +0100

    Update gradle to 9.2.1 and use java 25

--- src/main/java/org/bohverkill/models/Pair.java
+++ src/main/java/org/bohverkill/models/Pair.java
@@ -1,7 +1,21 @@
 package org.bohverkill.models;
 
+import org.bohverkill.utils.AssertionUtils;
+
+import java.util.List;
+
 public record Pair<A, B>(A a, B b) {
     public static <A, B> Pair<A, B> of(A a, B b) {
         return new Pair<>(a, b);
+    }
+
+    public static <A> Pair<A, A> of(List<A> list) {
+        AssertionUtils.assertEquals(2, list.size());
+        return new Pair<>(list.getFirst(), list.getLast());
+    }
+
+    public static <A> Pair<A, A> of(A[] array) {
+        AssertionUtils.assertEquals(2, array.length);
+        return new Pair<>(array[0], array[1]);
     }
 }
--- src/main/java/org/bohverkill/utils/CollectionUtils.java
+++ src/main/java/org/bohverkill/utils/CollectionUtils.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Gatherers;
 
 public final class CollectionUtils {
 
@@ -27,19 +27,17 @@ public final class CollectionUtils {
     }
 
     public static <T> List<Pair<T, T>> pairs(List<T> list) {
-        // NOTE: can be implemented with Stream Gatherers in the future, once JEP 473 is finalized
-        // return list.stream().gather(Gatherers.windowSliding(2)).toList();
-        return IntStream.range(0, list.size() - 1).mapToObj(i -> Pair.of(list.get(i), list.get(i + 1))).toList();
+        return list.stream().gather(Gatherers.windowSliding(2)).map(Pair::of).toList();
     }
 
     public static <K, V> void putOrApply(final Map<K, V> map, final K key, final V value, BinaryOperator<V> applyOperator) {
         map.compute(key, (_, v) -> v == null ? value : applyOperator.apply(v, value));
     }
 
-    public static <T> List<T> fixedList(T... a) {
-//        return Collections.unmodifiableList(Arrays.asList(a));
-        return List.of(a);
-    }
+//    public static <T> List<T> fixedList(T... a) {
+////        return Collections.unmodifiableList(Arrays.asList(a));
+//        return List.of(a);
+//    }
 
     public static <T> List<List<T>> copyList(final List<List<T>> src) {
         return src.stream().map(ArrayList::new).collect(Collectors.toCollection(() -> new ArrayList<>(src.size())));
